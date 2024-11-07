6d57ca1663beca93c5f67214e584d6ce465526a5
solution-files <admin@dotnetadmin.com>
2024-11-07 12:06:25 -0500

    Password update now functions correctly for local authentication

--- DNA/Program.cs
+++ DNA/Program.cs
@@ -108,9 +108,9 @@ services.AddControllersWithViews().ConfigureApplicationPartManager(apm => apm.Ap
 //    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
 //
 services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
 }).AddCookie(options => {
     options.LoginPath = "/Dashboard/Login";
     options.LogoutPath = "/Dashboard/Logout";
--- DNA3/DNA3/Program.cs
+++ DNA3/DNA3/Program.cs
@@ -109,9 +109,9 @@ services.AddControllersWithViews().ConfigureApplicationPartManager(apm => apm.Ap
 //    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
 //
 services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 }).AddCookie(options => {
     options.LoginPath = "/Dashboard/Login";
     options.LogoutPath = "/Dashboard/Logout";
